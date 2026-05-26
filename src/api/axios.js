107f8f1e59a11ed6e71013b768b9d2e6728401e8
Roopesh V <roopesh.v1595@gmail.com>
2026-05-26 19:06:20 +0530

    Update frontend API URL for production

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://backend-9xe8.onrender.com/api/v1",
 });
 
 export default api;
